4255a4d3c12ef880340a9d58895b3e19a9bda099
heg0m <hectorfg@gmail.com>
2015-11-03 10:29:23 -0500

    Primera versión proyecto de prueba

---
+++ AngularMVCTest/AngularMVCTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularMVCTest.Startup))]
+namespace AngularMVCTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
