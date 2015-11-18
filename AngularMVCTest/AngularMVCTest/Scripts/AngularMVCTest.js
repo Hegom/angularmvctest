678876975b9bc68f8d91f2c92cc4a4a34c796616
heg0m <hectorfg@gmail.com>
2015-11-18 16:30:07 -0500

    Tutorial Angular MVC

--- AngularMVCTest/AngularMVCTest/Scripts/AngularMVCTest.js
+++ AngularMVCTest/AngularMVCTest/Scripts/AngularMVCTest.js
@@ -1,3 +1,19 @@
-﻿var AngularMVCTest = angular.module('AngularMVCTest', []);
+﻿var AngularMVCTest = angular.module('AngularMVCTest', ['ngRoute']);
 
 AngularMVCTest.controller('LandingPageController', LandingPageController);
+
+var configFunction = function ($routeProvider) {
+    $routeProvider.
+        when('/routeOne', {
+            templateUrl: 'routesDemo/one'
+        })
+        .when('/routeTwo', {
+            templateUrl: 'routesDemo/two'
+        })
+        .when('/routeThree', {
+            templateUrl: 'routesDemo/three'
+        });
+}
+configFunction.$inject = ['$routeProvider'];
+
+AngularMVCTest.config(configFunction);
